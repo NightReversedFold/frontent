030fa77c545dca845b403c8f7ff25c949446c313
IlogicaAutomatizacion <automatizacion@ilogica-soluciones.cl>
2025-05-21 22:51:14 -0600

    'prueba22'

--- src/components/VideoViewport.tsx
+++ src/components/VideoViewport.tsx
@@ -1,9 +1,22 @@
+import { video } from "framer-motion/client";
+import { useRef,useEffect } from "react";
+
 export default ({ response }: { response: string }) => {
   console.log(response);
 
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (videoRef.current && response) {
+      videoRef.current.load(); // Forzamos que el video se recargue
+      console.log("Video blob assigned and loaded:", response);
+    }
+  }, [response]);
+
+
   return (
     <div>
-      <video key={response} controls width={600} src={response}>
+      <video ref={videoRef} key={response} controls width={600} src={response}>
         AAAAAAA
       </video>
       <a href={response} download="test.mp4">
